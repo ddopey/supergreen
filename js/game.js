c2ec54b392fd266c7ca6a64dd7ffa54372c5f9fa
ddopey <dopey@inbox.ru>
2020-05-01 22:31:40 +0300

    another try
    another try

--- js/game.js
+++ js/game.js
@@ -499,28 +499,42 @@ game.hero_moving = function(event) {
   if((x > arrows.x)&&(x < (arrows.x + 3 * cellSide))&&(y > (arrows.y + 3 * cellSide))&&(y < (arrows.y + 6 * cellSide))) {
     inputStates.left = true;
     game.updateposition();
+	 if(game.walking) {
+    interval = setTimeout(game.hero_moving.bind(null, event), 60);
+  }
   }
 
   //what happens if we press the right arrow
-  if((x > (arrows.x + 6 * cellSide))&&(x < (arrows.x + 9 * cellSide))&&(y > (arrows.y + 3 * cellSide))&&(y < (arrows.y + 6 * cellSide))) {
+   else if((x > (arrows.x + 6 * cellSide))&&(x < (arrows.x + 9 * cellSide))&&(y > (arrows.y + 3 * cellSide))&&(y < (arrows.y + 6 * cellSide))) {
     inputStates.right = true;
     game.updateposition();
+	     if(game.walking) {
+    interval = setTimeout(game.hero_moving.bind(null, event), 60);
+  }
   }
 
   //what happens if we press the up arrow
-  if((x > (arrows.x + 3 * cellSide))&&(x < (arrows.x + 6 * cellSide))&&(y > arrows.y)&&(y < (arrows.y + 3 * cellSide))) {
+  else if((x > (arrows.x + 3 * cellSide))&&(x < (arrows.x + 6 * cellSide))&&(y > arrows.y)&&(y < (arrows.y + 3 * cellSide))) {
     inputStates.up = true;
     game.updateposition();
+	 if(game.walking) {
+    interval = setTimeout(game.hero_moving.bind(null, event), 60);
+  }
   }
 
   //what happens if we press the down arrow
-  if((x > (arrows.x + 3 * cellSide))&&(x < (arrows.x + 6 * cellSide))&&(y > (arrows.y + 6 * cellSide))&&(y < (arrows.y + 9 * cellSide))) {
+   else if((x > (arrows.x + 3 * cellSide))&&(x < (arrows.x + 6 * cellSide))&&(y > (arrows.y + 6 * cellSide))&&(y < (arrows.y + 9 * cellSide))) {
     inputStates.down = true;
     game.updateposition();
+	     if(game.walking) {
+    interval = setTimeout(game.hero_moving.bind(null, event), 60);
   }
+  }
+	/*
   if(game.walking) {
     interval = setTimeout(game.hero_moving.bind(null, event), 60);
   }
+	*/
 }
  
 //moving the player with arrows on upper canvas
