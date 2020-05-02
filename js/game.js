7afc8c12f4c9806df2fc7c46b2896cecaf97606c
ddopey <dopey@inbox.ru>
2020-05-02 23:58:39 +0300

    strange
    strange

--- js/game.js
+++ js/game.js
@@ -519,12 +519,12 @@ game.hero_moving = function(event) {
     game.updateposition();
   }
 
-//let func = game.hero_moving.bind(null, event);
+let func = game.hero_moving.bind(null, event);
 	
   if(game.walking) {
 	//timer = setTimeout(func, 60);
 	 // timer = setTimeout(function(){game.hero_moving(event);}, 60);
-	setTimeout(game.hero_moving.bind(null, event), 60);
+	setTimeout(func, 60);
   }
 
 }
